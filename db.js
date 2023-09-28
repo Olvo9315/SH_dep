5bfc9d8e188098f50de9dfb2714130ecd8d2226a
olvo <alexvolenko9315@gmail.com>
2023-09-28 12:03:40 +0000

    lopd y db a 0 0 0 0 8080

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb://127.0.0.1:27017";
+const uri = "mongodb://0.0.0.0:27017";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 //client.connect(error => {
